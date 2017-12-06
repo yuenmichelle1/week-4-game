d2c93676a21823ec0a010be99ee2104acc1b5795
Marylin <marylin@Marylins-MacBook-Pro.local>
2017-12-06 13:14:14 -0600

    getting defenders and player character

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,40 +26,54 @@ $("#kyloHP").html(kylo.HP);
 $("#vaderHP").html(vader.HP);
 //when clicked move playerChoice to 'Your Character and move others to Enemies Availavle to attack'
 var flag=0;
+var check =0; 
 $("#yoda").on("click", function(){
-	flag=1;
-	if (flag === 1){
+	if (flag !=1 && flag !=0) {
+		$("#Enemy").append($("#yoda"));
+	}
+	 else if (flag ===0){
+	 	flag=1;
 		$("#playerChoice").append($("#yoda"));
-		$("#availableEnemies").append($("#princess"));
-		$("#availableEnemies").append($("#ben"));
-		$("#availableEnemies").append($("#anikin"));
+		$("#availableEnemies").append($("#princess"),$("#ben"),$("#anikin"));
+		$("#availableEnemies .img-box").addClass("redEnemy");
+		$("#playerChoice .img-box").addClass("whiteBox");
 	}
 });
 $("#princess").on("click", function(){
-	flag=2;
-	if (flag===2){
-		$("#playerChoice").append($("#princess"));
-		$("#availableEnemies").append($("#yoda"));
-		$("#availableEnemies").append($("#ben"));
-		$("#availableEnemies").append($("#anikin"));
+	if (flag !=2 && flag !=0){
+		$("#Enemy").append($("#princess"));
 	}
+	else if (flag ===0){
+		flag=2;
+		$("#playerChoice").append($("#princess"));
+		$("#availableEnemies").append($("#yoda"), $("#ben"),$("#anikin"));
+		$("#availableEnemies .img-box").addClass("redEnemy");
+		$("#playerChoice .img-box").addClass("whiteBox");
+	}	
 });
 $("#ben").on("click", function(){
+	if (flag !=3 && flag !=0){
+		$("#Enemy").append($("#ben"));
+	}else if (flag===0){
 	flag=3;
-	if (flag===3){
-		$("#playerChoice").append($("#ben"));
-		$("#availableEnemies").append($("#yoda"));
-		$("#availableEnemies").append($("#princess"));
-		$("#availableEnemies").append($("#anikin"));
+	$("#playerChoice").append($("#ben"));
+	$("#availableEnemies").append($("#yoda"), $("#princess"), $("#anikin"));
+	$("#availableEnemies .img-box").addClass("redEnemy");
+	$("#playerChoice .img-box").addClass("whiteBox");
 	}
 });
 $("#anikin").on("click",function(){
+	if (flag !=4 && flag !=0){
+		$("#Enemy").append($("#anikin"));
+	}else if (flag===0){
 	flag=4;
-	if (flag ===4){
-		$("#playerChoice").append($("#anikin"));
-		$("#availableEnemies").append($("#yoda"));
-		$("#availableEnemies").append($("#princess"));
-		$("#availableEnemies").append($("#ben"));		
+	$("#playerChoice").append($("#anikin"));
+	$("#availableEnemies").append($("#yoda"), $("#princess"),$("#ben"));
+	$("#availableEnemies .img-box").addClass("redEnemy");
+	$("#playerChoice .img-box").addClass("whiteBox");		
 	}
 });
-//if (flag===1)
+
+$(".button").on("click", function(){
+	
+})
